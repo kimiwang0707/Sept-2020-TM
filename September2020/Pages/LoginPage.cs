0788f5a02be368c3063b36b6091baedbd262ef20
kimiwang0707 <kimiwang0707@gmail.com>
2020-09-22 15:49:45 +1200

    added a comment

--- September2020/Pages/LoginPage.cs
+++ September2020/Pages/LoginPage.cs
@@ -8,6 +8,7 @@ namespace September2020.Pages
     {
         public void LoginSteps(IWebDriver driver)
         {
+            // test
             //Lauch browser and enter the URL
             driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
 
