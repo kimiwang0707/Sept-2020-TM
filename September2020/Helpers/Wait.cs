46a4e8974228ae2c42a6907c2ed67d766fd4572f
kimiwang0707 <kimiwang0707@gmail.com>
2020-09-27 15:45:12 +1300

    Update wait syntax

--- September2020/Helpers/Wait.cs
+++ September2020/Helpers/Wait.cs
@@ -7,7 +7,7 @@ namespace September2020.Helpers
 {
     class Wait
     {
-        
+        // Write wait syntax for Element EXIST condition.
         public static void WaitForElement(IWebDriver driver, string key, string value)
         {
             try
@@ -36,7 +36,7 @@ namespace September2020.Helpers
 
 
 
-        // Can set waiting time in this constructor
+        // Write wait syntax for Element VISIBLE condition (Can set waiting time in this constructor)
         public static void WaitForElementVisibility(IWebDriver driver, string key, string value, int seconds)
         {
             try
@@ -65,6 +65,32 @@ namespace September2020.Helpers
 
         }
 
+
+        // Write wait syntax for Element CLICKABLE condition
+        public static void WaitForElementClickable(IWebDriver driver, string key, string value)
+        {
+            var wait = new WebDriverWait(driver, new TimeSpan(0, 0, 5));
+
+            try
+            {
+                if (key == "XPath")
+                {
+                    wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath(value)));
+                }
+                if (key == "Id")
+                {
+                    wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id(value)));
+                }
+                if (key == "CssSelector")
+                {
+                    wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.CssSelector(value)));
+                }
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Element failed to be clickable!", ex.Message);
+            }
+        }
 
     }
 }
--- September2020/Pages/CompanyPage.cs
+++ September2020/Pages/CompanyPage.cs
@@ -12,61 +12,66 @@ namespace September2020.Pages
         // *** Create  company record ***
         public void CreateCompany(IWebDriver driver)
         {
+            try
+            {
+                //Click Create New
+                Wait.WaitForElement(driver, "XPath", "//*[@id='container']/p/a");
+                driver.FindElement(By.XPath("//*[@id='container']/p/a")).Click();
 
-            //Click Create New
-            Wait.WaitForElement(driver, "XPath", "//*[@id='container']/p/a");
-            driver.FindElement(By.XPath("//*[@id='container']/p/a")).Click();
+                //Input name
+                Wait.WaitForElement(driver, "Id", "Name");
+                IWebElement Name = driver.FindElement(By.Id("Name"));
+                Name.SendKeys("Super Star Company");
 
-            //Input name
-            Wait.WaitForElement(driver, "Id", "Name");
-            IWebElement Name = driver.FindElement(By.Id("Name"));
-            Name.SendKeys("Super Star Company");
+                // Click Edit to input contact
+                driver.FindElement(By.Id("EditContactButton")).Click();
+                Thread.Sleep(500);
 
-            // Click Edit to input contact
-            driver.FindElement(By.Id("EditContactButton")).Click();
-            Thread.Sleep(500);
+                // Switch to popup window
+                IWebElement iframe = driver.FindElement(By.ClassName("k-content-frame"));
+                driver.SwitchTo().Frame(iframe);
 
-            // Switch to popup window
-            IWebElement iframe = driver.FindElement(By.ClassName("k-content-frame"));
-            driver.SwitchTo().Frame(iframe);
+                // Input data
+                Wait.WaitForElement(driver, "Id", "FirstName");
+                driver.FindElement(By.Id("FirstName")).SendKeys("Taylor");
+                driver.FindElement(By.Id("LastName")).SendKeys("Swift");
+                driver.FindElement(By.Id("PreferedName")).SendKeys("Taytay");
+                driver.FindElement(By.Id("Phone")).SendKeys("911");
+                driver.FindElement(By.Id("Mobile")).SendKeys("110");
+                driver.FindElement(By.Id("email")).SendKeys("taylorswift@gmail.com");
+                driver.FindElement(By.Id("autocomplete")).SendKeys("test address");
+                driver.FindElement(By.Id("Street")).SendKeys("Taiping Street");
+                driver.FindElement(By.Id("City")).SendKeys("Auckland");
+                driver.FindElement(By.Id("Postcode")).SendKeys("1055");
+                driver.FindElement(By.Id("Country")).SendKeys("New Zealand");
 
-            // Input data
-            Wait.WaitForElement(driver, "Id", "FirstName");
-            driver.FindElement(By.Id("FirstName")).SendKeys("Taylor");
-            driver.FindElement(By.Id("LastName")).SendKeys("Swift");
-            driver.FindElement(By.Id("PreferedName")).SendKeys("Taytay");
-            driver.FindElement(By.Id("Phone")).SendKeys("911");
-            driver.FindElement(By.Id("Mobile")).SendKeys("110");
-            driver.FindElement(By.Id("email")).SendKeys("taylorswift@gmail.com");
-            driver.FindElement(By.Id("autocomplete")).SendKeys("test address");
-            driver.FindElement(By.Id("Street")).SendKeys("Taiping Street");
-            driver.FindElement(By.Id("City")).SendKeys("Auckland");
-            driver.FindElement(By.Id("Postcode")).SendKeys("1055");
-            driver.FindElement(By.Id("Country")).SendKeys("New Zealand");
+                // Click Save Contact to create new record
+                driver.FindElement(By.Id("submitButton")).Click();
 
-            // Click Save Contact to create new record
-            driver.FindElement(By.Id("submitButton")).Click();
+                // Return default main window
+                driver.SwitchTo().DefaultContent();
 
-            // Return default main window
-            driver.SwitchTo().DefaultContent();
+                // Save button
+                Wait.WaitForElement(driver, "Id", "SaveButton");
+                driver.FindElement(By.Id("SaveButton")).Click();
 
-            // Save button
-            Wait.WaitForElement(driver, "Id", "SaveButton");
-            driver.FindElement(By.Id("SaveButton")).Click();
+                //Scroll to bottom
 
-            //Scroll to bottom
+                // Go to last page
+                Wait.WaitForElement(driver, "XPath", "//*[@id='companiesGrid']/div[4]/a[4]/span");
+                driver.FindElement(By.XPath("//*[@id='companiesGrid']/div[4]/a[4]/span")).Click();
 
-            // Go to last page
-            Wait.WaitForElement(driver, "XPath", "//*[@id='companiesGrid']/div[4]/a[4]/span");
-            driver.FindElement(By.XPath("//*[@id='companiesGrid']/div[4]/a[4]/span")).Click();
+                // validate if the company record is added to the list
+                Wait.WaitForElementVisibility(driver, "XPath", "//*[@id='companiesGrid']/div[3]/table/tbody/tr[last()]/td[1]", 5);
+                IWebElement lastItem = driver.FindElement(By.XPath("//*[@id='companiesGrid']/div[3]/table/tbody/tr[last()]/td[1]"));
 
-            // validate if the company record is added to the list
-            Wait.WaitForElementVisibility(driver, "XPath", "//*[@id='companiesGrid']/div[3]/table/tbody/tr[last()]/td[1]", 5);
-            IWebElement lastItem = driver.FindElement(By.XPath("//*[@id='companiesGrid']/div[3]/table/tbody/tr[last()]/td[1]"));
-
-            //Use Assert syntax to judge fail or pass
-            Assert.That(lastItem.Text, Is.EqualTo("Super Star Company"));
-            
+                //Use Assert syntax to judge fail or pass
+                Assert.That(lastItem.Text, Is.EqualTo("Super Star Company"));
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Fail to create company", ex.Message);
+            }
         }
 
 
@@ -74,26 +79,32 @@ namespace September2020.Pages
         // *** Edit company record ***
         public void EditCompany(IWebDriver driver)
         {
-            // Click the 1st row item to edit
-            Wait.WaitForElement(driver, "XPath", "//*[@id='companiesGrid']/div[3]/table/tbody/tr[1]/td[3]/a[1]");
-            driver.FindElement(By.XPath("//*[@id='companiesGrid']/div[3]/table/tbody/tr[1]/td[3]/a[1]")).Click();
+            try
+            {
+                // Click the 1st row item to edit
+                Wait.WaitForElement(driver, "XPath", "//*[@id='companiesGrid']/div[3]/table/tbody/tr[1]/td[3]/a[1]");
+                driver.FindElement(By.XPath("//*[@id='companiesGrid']/div[3]/table/tbody/tr[1]/td[3]/a[1]")).Click();
 
-            // Input new data
-            Wait.WaitForElement(driver, "Id", "Name");
-            driver.FindElement(By.Id("Name")).Clear();
-            driver.FindElement(By.Id("Name")).SendKeys("Donald Trump");
+                // Input new data
+                Wait.WaitForElement(driver, "Id", "Name");
+                driver.FindElement(By.Id("Name")).Clear();
+                driver.FindElement(By.Id("Name")).SendKeys("Donald Trump");
 
-            // Click Save Company
-            Wait.WaitForElement(driver, "Id", "SaveButton");
-            driver.FindElement(By.Id("SaveButton")).Click();
+                // Click Save Company
+                Wait.WaitForElement(driver, "Id", "SaveButton");
+                driver.FindElement(By.Id("SaveButton")).Click();
 
-            // Validate if the item was updated
-            Wait.WaitForElement(driver, "XPath", "//*[@id='companiesGrid']/div[3]/table/tbody/tr[1]/td[1]");
-            IWebElement FirstRowName = driver.FindElement(By.XPath("//*[@id='companiesGrid']/div[3]/table/tbody/tr[1]/td[1]"));
+                // Validate if the item was updated
+                Wait.WaitForElement(driver, "XPath", "//*[@id='companiesGrid']/div[3]/table/tbody/tr[1]/td[1]");
+                IWebElement FirstRowName = driver.FindElement(By.XPath("//*[@id='companiesGrid']/div[3]/table/tbody/tr[1]/td[1]"));
 
-            //Use Assert syntax to judge fail or pass
-            Assert.That(FirstRowName.Text, Is.EqualTo("Donald Trump"));
-            
+                //Use Assert syntax to judge fail or pass
+                Assert.That(FirstRowName.Text, Is.EqualTo("Donald Trump"));
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Fail to edit company", ex.Message);
+            }
         }
 
 
@@ -105,18 +116,18 @@ namespace September2020.Pages
             {
                 // * Validate the alert window with cancel operation *
                 // Record the 1st row item
-                Wait.WaitForElement(driver, "XPath", "//*[@id='companiesGrid']/div[3]/table/tbody/tr[1]/td[1]");
+                Wait.WaitForElementClickable(driver, "XPath", "//*[@id='companiesGrid']/div[3]/table/tbody/tr[1]/td[3]/a[2]");
                 IWebElement FirstItemName = driver.FindElement(By.XPath("//*[@id='companiesGrid']/div[3]/table/tbody/tr[1]/td[1]"));
 
                 // Click the 1st item to delete
                 driver.FindElement(By.XPath("//*[@id='companiesGrid']/div[3]/table/tbody/tr[1]/td[3]/a[2]")).Click();
 
                 // Click cancel
-                Thread.Sleep(1000);
+                Thread.Sleep(500);
                 driver.SwitchTo().Alert().Dismiss();
 
                 // Locate the item deleted
-                Wait.WaitForElement(driver, "XPath", "//*[@id='companiesGrid']/div[3]/table/tbody/tr[1]/td[1]");
+                Thread.Sleep(1500);
                 IWebElement deleteItem = driver.FindElement(By.XPath("//*[@id='companiesGrid']/div[3]/table/tbody/tr[1]/td[1]"));
 
                 // Use assert syntax to judge if pass of fail
@@ -126,7 +137,8 @@ namespace September2020.Pages
                 // * Validate the alert window with OK to delete *
 
                 // Recond second row data
-                IWebElement SecondItemName = driver.FindElement(By.XPath("//*[@id='companiesGrid']/div[3]/table/tbody/tr[2]/td[1]"));
+                Wait.WaitForElementClickable(driver, "XPath", "//*[@id='companiesGrid']/div[3]/table/tbody/tr[1]/td[3]/a[2]");
+                var SecondItemName = driver.FindElement(By.XPath("//*[@id='companiesGrid']/div[3]/table/tbody/tr[2]/td[1]")).Text;
 
                 // click delete again
                 driver.FindElement(By.XPath("//*[@id='companiesGrid']/div[3]/table/tbody/tr[1]/td[3]/a[2]")).Click();
@@ -136,10 +148,11 @@ namespace September2020.Pages
                 driver.SwitchTo().Alert().Accept();
 
                 // Validate if the second row change to first row
-                Wait.WaitForElement(driver, "XPath", "//*[@id='companiesGrid']/div[3]/table/tbody/tr[1]/td[1]");
-                IWebElement FirstItemName1 = driver.FindElement(By.XPath("//*[@id='companiesGrid']/div[3]/table/tbody/tr[1]/td[1]"));
+                // Cannot use ----Wait.WaitForElement(driver, "XPath", "//*[@id='companiesGrid']/div[3]/table/tbody/tr[1]/td[1]");
+                Thread.Sleep(1500);
+                var FirstItemName1 = driver.FindElement(By.XPath("//*[@id='companiesGrid']/div[3]/table/tbody/tr[1]/td[1]")).Text;
 
-                Assert.That(FirstItemName1.Text, Is.EqualTo(SecondItemName.Text));             
+                Assert.That(FirstItemName1, Is.EqualTo(SecondItemName));             
 
             }
             catch (Exception ex)
